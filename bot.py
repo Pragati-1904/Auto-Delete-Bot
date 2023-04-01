16df81e002e03fddd8c6400b4709dc9d2f75d980
Pragati-1904 <112616265+Pragati-1904@users.noreply.github.com>
2023-04-01 13:39:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 from telethon import TelegramClient, Button, events
 from telethon.utils import get_peer_id
 from aioredis import Redis
-from .settings import *
-from .dbf import Dbf
+from settings import *
+from dbf import Dbf
 import asyncio, os
 
 ############## Client Setup ###############
